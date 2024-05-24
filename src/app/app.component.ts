5857927fbd745ea1c7b3754f58571a8611f2b48d
Asma Eleuchi <euchi.asma77@gmail.com>
2024-05-24 16:07:58 +0200

    Remonter les informations du panier de composant enfant(shop) vers composant parent(app)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,48 @@ import { count } from 'console';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent   {
   title = 'chocoPap';
+  cartCount: number = 0;
+  cart: any[] = [];
+  
+  //Recuperer les valeur stocker dans le stockage local
+  ngOnInit() {
+    const storedCartCount = localStorage.getItem('cartCount');
+    if (storedCartCount) {
+      this.cartCount = parseInt(storedCartCount, 10);
+    }
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+      this.cart = JSON.parse(storedCart);
+    }
+  }
+ 
+  //Mettre à jour l'état de panier et le synchroniser  avec le localStorage
+  updateCart(cart: any) {
+    this.cart = cart;
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
+//Mettre à jour la quantité des articles  et la synchroniser  avec le localStorage
+  updateCartCount(count: number) {
+    this.cartCount = count;
+    localStorage.setItem('cartCount', this.cartCount.toString());
+  }
+
+  //lier au evenements, permet de mettre à jour le panier en temps réel 
+  onActivate(event) {
+    if (event.cartChanged) {
+      event.cartChanged.subscribe(cart => {
+        this.updateCart(cart);
+      });
+    }
+
+    if (event.cartCountChanged) {
+      event.cartCountChanged.subscribe(count => {
+        this.updateCartCount(count);
+      });
+    }
+  }
+
+  
 }
